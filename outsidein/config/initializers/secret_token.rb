f14152b3002bf0a1c9798859ad336024d5f4b90b
Soo Philip Kim <philipjkim@gmail.com>
2012-06-06 17:24:18 +0900

    ran "rails new outsidein"

---
+++ outsidein/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outsidein::Application.config.secret_token = '6d7feaeb1c19f02425b90d2a62dac334a7c0c46916a212a0a3ee20ee15ab79a8cea08df3a5f5aaa38532498bb1181a965798b6d8c29704457af6c667a292b2e7'
